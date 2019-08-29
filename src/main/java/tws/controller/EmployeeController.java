5e17ac724566a950d79cb5e10e645c1050580f9c
yaoyunping <250815994@qq.com>
2019-08-29 17:01:41 +0800

    mybatiscrud

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -3,7 +3,9 @@ package tws.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +27,31 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> getAll() {
         return ResponseEntity.ok(employeeMapper.selectAll());
     }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee>selectById(@PathVariable String id){
+    	Employee employee = employeeMapper.selectById(id);
+    	return ResponseEnity.ok(employee);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee>updateById(
+    		@PathVariable String id,
+    		@RequestBody Employee employee
+    		){
+    	employeeMapper.updateById(id,employee);
+    	return ResponseEnity.ok(employee);
+    }
+    
+    
+    @PostMapping("")
+    public ResponseEntity<Employee>insert(@RequestBody Employee employee){
+    	String id = UUID.randomUUID().toString();
+    			employee.setId(id);
+    	employeeMapper.insert(employee);
+    	return ResponseEntity.created(URI.create("/employees"+id)).build();
+    }
+    
+    
 
 }
--- src/main/java/tws/repository/EmployeeMapper.java
+++ src/main/java/tws/repository/EmployeeMapper.java
@@ -1,6 +1,8 @@
 package tws.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import tws.entity.Employee;
 
 import java.util.List;
@@ -8,4 +10,7 @@ import java.util.List;
 @Mapper
 public interface EmployeeMapper {
     List<Employee> selectAll();
+    void insert(@Param ("employee")Employee employee);
+    Employee selectById(@Param("id")String id);
+	void updateById(@Param("id") String id, @Param ("employee")Employee employee);
 }
